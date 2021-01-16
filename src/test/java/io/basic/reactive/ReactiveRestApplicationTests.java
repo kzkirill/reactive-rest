a4143bdd5e6c94cd96f9fd5c1494ff47ade6e66c
kzkirill <kz.kirill@protonmail.com>
2021-01-16 18:24:49 +0200

    Add files via upload

---
+++ src/test/java/io/basic/reactive/ReactiveRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.basic.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
